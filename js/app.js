1b2ac5a249807f5d7afe0792958a2795039c0386
Pedro Lucas <izielucas2016@gmail.com>
2024-02-28 10:52:01 -0300

    updated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-const baseURL = 'https://api.magicthegathering.io/v1/'
+const baseURL = 'https://api.magicthegathering.io/v1'
+
+const loadCards = async () =>{
+    const res = await fetch(`${baseURL}/cards`)
+    const data = await res.json();
+    return await data
+}
+
+const loadAllWithPromiseAll = async () =>{
+    const [cards] = await Promise.all([
+        loadCards(),
+    ])
+
+    console.log(cards)
+    showCards(cards.results)
+}
+
+loadAllWithPromiseAll()
+
+function showCards(cards){
+    // const cardsContainer = document.getElementById("cards-container")
+    // cards.map((cards) =>{
+    //     console.log(cards.id)
+    // })
+}
