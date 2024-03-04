57991f59a90dc92b657bebf4cf1140bb4931285b
Pedro Lucas <izielucas2016@gmail.com>
2024-03-04 02:54:34 -0300

    finished... i think

--- js/card.js
+++ js/card.js
@@ -1,34 +1,53 @@
-const baseURL = 'https://api.magicthegathering.io/v1/cards';
+async function fetchAndDisplayCard() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const idParam = urlParams.get('id');
 
-async function loadCards(baseURL, id){
+    if (!idParam) {
+        console.error('ID not found');
+        return;
+    }
+
+    const baseURL = 'https://api.magicthegathering.io/v1/cards';
+
     try {
-        const response = await fetch(`${baseURL}/${id}`)
-        if(!response.ok){
-            throw new Error('Error trying to load the card')
+        const response = await fetch(`${baseURL}/${idParam}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch card');
         }
-        return await response.json()
-    } catch (error){
-        console.error(error)
-        throw error
+        const card = await response.json();
+
+        // Exibir as informações da carta
+        showCardDetails(card);
+    } catch (error) {
+        console.error('Error fetching card:', error);
     }
 }
 
-async function load(){
-    const urlParams = new URLSearchParams(window.location.search)
-    const idParam = urlParams.get('id')
+function showCardDetails(cardData) {
+    const cardContainer = document.getElementById('cards-container');
+    const divCards = document.createElement('div');
+    divCards.classList.add('card-box');
 
-    if(!idParam){
-        console.error('ID not found')
-    }
+    const cardDetails = `
+    <div class="card-details">
+        <div class="name-card">
+            <h3>${cardData.card.name}</h3>
+        </div>
+        <div class="cards-detail">
+            <p>Mana Cost: ${cardData.card.manaCost}</p>
+            <p>Rarity: ${cardData.card.rarity}</p>
+            <p>Type: ${cardData.card.type}</p>
+            <a href="https://api.magicthegathering.io/v1/sets/${cardData.card.set}">
+                <p>Sets: ${cardData.card.set}</p>
+            </a>
+        </div>
+        <div class='card-image'>
+            <img src="${cardData.card.imageUrl}" alt="Magic the Wiki logo">
+        </div>
+    </div>`;
+    divCards.innerHTML = cardDetails;
 
-    const urlBase = `https://api.magicthegathering.io/v1/cards`
-
-    try {
-        const card = await loadCards(urlBase, id)
-        console.log('card:', card)
-    }catch(error){
-        console.error('Error trying to load the card')
-    }
+    cardContainer.appendChild(divCards);
 }
 
-load()
+fetchAndDisplayCard();
